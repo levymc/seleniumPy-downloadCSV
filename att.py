e15f0eb4c8049aab5af0bc6d1781c8955d846a38
levymc <levymcruz@gmail.com>
2023-05-18 08:47:30 -0300

    criando o arquivo de atualização

---
+++ att.py
@@ -0,0 +1,11 @@
+# Make selenium and chromedriver work for Untappd.com
+
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
+
+#driver = webdriver.Chrome()
+driver = webdriver.Chrome(ChromeDriverManager().install())
+
+url = "https://untappd.com/"
+driver.get(url)
